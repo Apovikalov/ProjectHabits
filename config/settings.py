47ae78d9b6b234d94b3c1a92d7ec2c8a2bd5add5
Андрей Повикалов <apovikalov@bk.ru>
2026-09-03 18:45:40 +0600

    Docker Compose fixes

--- config/settings.py
+++ config/settings.py
@@ -24,8 +24,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ny$w4l0g91vx)fl($(azc%um#8bcw1)&ye%($ntfq8(x^ow5x&'
-# 'django-insecure-ny$w4l0g91vx)fl($(azc%um#8bcw1)&ye%($ntfq8(x^ow5x&'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
